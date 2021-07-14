26ba3f851292a4211aed3edf4953d4ff2795a148
Alexander <ya.alexandersmirnov@yandex.ru>
2021-07-14 22:14:58 +0300

    0.9

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -2,7 +2,6 @@ import shutil
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.http import request
 from django.test import Client, TestCase
 from django.urls import reverse
 from django.core.cache import cache
